0ac2a1f1204f5edcd8bbe716d697ccf104d2eec5
Zuri <zurilanegriffore@cmail.carleton.ca>
2024-09-20 11:26:37 -0400

    Changed BuddyInfo parameters, constructor, getters, and setters. Now stores address and phone number opposed to age.

---
+++ Lab 2 Project/src/BuddyInfo.java
@@ -0,0 +1,36 @@
+public class BuddyInfo {
+
+    private String name;
+    private String address;
+    private String phoneNumber;
+
+    public BuddyInfo(String name, String address, String phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
